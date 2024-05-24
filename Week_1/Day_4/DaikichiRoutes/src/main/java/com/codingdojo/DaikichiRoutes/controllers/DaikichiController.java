ad3ee473c289e8df45832fbd9e6dd646944ee6b2
louai60 <louaysaafi60@gmail.com>
2024-05-24 19:36:27 +0000

    Java

---
+++ Week_1/Day_4/DaikichiRoutes/src/main/java/com/codingdojo/DaikichiRoutes/controllers/DaikichiController.java
@@ -0,0 +1,2 @@
+package com.codingdojo.DaikichiRoutes.controllers;public class DaikichiController {
+}
---
+++ Week_1/Day_4/DaikichiRoutes/src/main/java/com/louaysaafi/DaikichiRoutes/DaikichiController.java
@@ -0,0 +1,4 @@
+package com.louaysaafi.DaikichiRoutes;
+
+public class DaikichiController {
+}
