5c6885c5658c5425732a084f27a7406621486214
louai60 <louaysaafi60@gmail.com>
2024-05-24 19:56:55 +0000

    Java

--- Week_1/Day_4/DaikichiRoutes/src/main/java/com/louaysaafi/DaikichiRoutes/DaikichiController.java
+++ Week_1/Day_4/DaikichiRoutes/src/main/java/com/louaysaafi/DaikichiRoutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.louaysaafi.DaikichiRoutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,19 @@ public class DaikichiController {
     @RequestMapping("/tomorrow")
     public String tomorrow() {
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+
+    @RequestMapping("/travel/{city}")
+    public String travelToCity(@PathVariable("city") String city) {
+        return "Congratulations! You will soon travel to " + city + "!";
+    }
+
+    @RequestMapping("/lotto/{number}")
+    public String checkLottoNumber(@PathVariable("number") int number) {
+        if (number % 2 == 0) {
+            return "You will take a grand journey in the near future, but be wary of tempting offers.";
+        } else {
+            return "You have enjoyed the fruits of your labor, but now is a great time to spend time with family and friends.";
+        }
     }
 }
